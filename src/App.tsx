df458057b92f9d79c09db1b4447800784cf89497
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-07-09 00:36:26 -0300

    iniciando a estilização do campo de busca

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { FiMapPin } from "react-icons/fi";
+import { CiCalendar } from "react-icons/ci";
+
 export function App() {
 
   return (
@@ -5,10 +8,17 @@ export function App() {
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
         <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
         
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center">
-          <input type="text" placeholder="para onde você vai?" />
-          <input type="text" placeholder="quando?" />
-          <button>Continuar</button>
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape">
+          <div className="flex items-center gap-2 flex-1" >
+            <FiMapPin className="size-5 text-zinc-400"/>
+            <input type="text" placeholder="para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400"/>
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <CiCalendar className="size-5 text-zinc-400"/>
+            <input type="text" placeholder="quando?" className="bg-transparent text-lg placeholder-zinc-400"/>
+          </div>
+          <button className="bg-lime-300 text-lime-950 rounded-lg py-2 px-5">Continuar</button>
         </div>
 
         <p className="text-sm text-zinc-500">Ao planejar sua viagem pela plann.er você automaticamente concorda <br/>
