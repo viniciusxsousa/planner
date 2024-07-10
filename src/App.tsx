d7e99b644e40eb846fe2ddad5874081fbecd18f1
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-07-10 15:53:10 -0300

    criando o modal de confirmação

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { FormEvent, useState } from "react";
 
 import { FiMapPin, FiUserPlus, FiAtSign } from "react-icons/fi";
-import { CiCalendar } from "react-icons/ci";
+import { CiCalendar, CiUser, CiMail } from "react-icons/ci";
 import { GoArrowRight, GoPlus } from "react-icons/go";
 import { IoMdSwitch, IoMdClose } from "react-icons/io";
 
 export function App() {
   const [isGuestsInputOpen, setIsGuestInputopen] = useState(false);
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
-  const [emailsToGuest, setEmailsToGuest] = useState([
-    'vinicius@mail.com'
-  ])
+  const [isConfirmTravelModalOpen, setIsconfirmTravelModalOpen] = useState(false);
+  const [emailsToGuest, setEmailsToGuest] = useState([])
 
 
   function openGuestsInput() {
@@ -29,13 +28,21 @@ export function App() {
     setIsGuestsModalOpen(false);
   }
 
+  function openConfirmTravelModal() {
+    setIsconfirmTravelModalOpen(true);
+  }
+
+  function closeConfirmTravelModal() {
+    setIsconfirmTravelModalOpen(false);
+  }
+
   function addNewEmailToEnvite(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    
+
     const data = new FormData(event.currentTarget);
     const email: any = data.get('email');
 
-    if(emailsToGuest.includes(email)) {
+    if (emailsToGuest.includes(email)) {
       return
     }
 
@@ -45,7 +52,7 @@ export function App() {
   }
 
   function removeGuestEmail(email: string) {
-    const listGuestUpdate = emailsToGuest.filter( guest =>  guest != email);
+    const listGuestUpdate = emailsToGuest.filter(guest => guest != email);
 
     setEmailsToGuest(listGuestUpdate);
   }
@@ -95,7 +102,7 @@ export function App() {
                 <span className="bg-transparent text-lg placeholder-zinc-400 outline-none" >Quem estará na viagem?</span>
               </button>
 
-              <button className="bg-lime-300 text-lime-950 rounded-lg py-2 px-5 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <button onClick={openConfirmTravelModal} className="bg-lime-300 text-lime-950 rounded-lg py-2 px-5 font-medium flex items-center gap-2 hover:bg-lime-400">
                 Confirmar viagem
                 <GoArrowRight className="size-5" />
               </button>
@@ -125,7 +132,7 @@ export function App() {
                   emailsToGuest.map(email => (
                     <div key={email} className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
                       <span>{email}</span>
-                      <button onClick={ () => removeGuestEmail(email) }  ><IoMdClose className="size-4 text-zinc-400" /></button>
+                      <button onClick={() => removeGuestEmail(email)}  ><IoMdClose className="size-4 text-zinc-400" /></button>
                     </div>
                   ))
                 }
@@ -147,6 +154,46 @@ export function App() {
             </div>
           </div>
         )
+      }
+
+      {
+        isConfirmTravelModalOpen && (
+          <div className="fixed inset-0 bg-black/60 flex items-center justify-center" >
+            <div className="bg-zinc-900 w-[640px] rounded-xl py-5 px-6 shadow-shape space-y-5">
+
+              <div className="space-y-2">
+                <div className="flex justify-between items-center">
+                  <p className="text-lg font-semibold" >Confirmar criação da viagem</p>
+                  <button onClick={closeConfirmTravelModal} ><IoMdClose className="size-6" /></button>
+                </div>
+                <p className="text-sm text-zinc-400" >Para concluir a criação da viagem para <span className="font-semibold text-zinc-100" >Florianópolis, Brasil</span> nas datas de <span className="font-semibold text-zinc-100">16 a 27 de Agosto de 2024</span>, preencha seus dados abaixo:</p>
+              </div>
+
+              <form onSubmit={addNewEmailToEnvite} className="space-y-3">
+
+                <div className="py-2.5 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+
+                  <CiUser className="size-5 text-zinc-100" />
+                  <input type="text" name='name' placeholder="Seu nome completo" className="w-full bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+
+                </div>
+
+                <div className="py-2.5 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+
+                  <CiMail className="size-5 text-zinc-100" />
+                  <input type="email" name='email' placeholder="Seu e-mail pessoal" className="w-full bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+
+                </div>
+
+                <button className="w-full bg-lime-300 text-lime-950 rounded-lg py-1.5 h-11 flex items-center justify-center hover:bg-lime-400">
+                  <span>Confirmar criação da viagem</span>
+                </button>
+
+              </form>
+            </div>
+          </div>
+        )
+
       }
 
     </div>
