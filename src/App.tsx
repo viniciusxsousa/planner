3492df24989e5d3ab9be11977621e718f84e14c3
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-07-10 16:16:51 -0300

    fazendo o contador para pessoas convidadas

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,13 @@ export function App() {
 
               <button onClick={openGuestsModal} className="flex items-center gap-3 flex-1" >
                 <FiUserPlus className="size-5 text-zinc-400" />
-                <span className="bg-transparent text-lg placeholder-zinc-400 outline-none" >Quem estará na viagem?</span>
+                { 
+                  emailsToGuest.length > 0 ? (
+                    <span className="bg-transparent text-lg placeholder-zinc-400 outline-none" >{emailsToGuest.length} pessoa(s) convidada(s)</span>
+                  ) : (
+                    <span className="bg-transparent text-lg placeholder-zinc-400 outline-none" >Quem estará na viagem?</span>
+                  ) 
+                }
               </button>
 
               <button onClick={openConfirmTravelModal} className="bg-lime-300 text-lime-950 rounded-lg py-2 px-5 font-medium flex items-center gap-2 hover:bg-lime-400">
