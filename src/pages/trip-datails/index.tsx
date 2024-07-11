cd4c3212efec3fcc5e5bc4e229783eea564e76f7
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-07-11 17:10:28 -0300

    finalizando a estrutura básica do details

--- src/pages/trip-datails/index.tsx
+++ src/pages/trip-datails/index.tsx
@@ -1,5 +1,6 @@
 import { FiMapPin, FiPlus } from "react-icons/fi";
-import { IoMdSwitch, IoIosCheckmarkCircleOutline } from "react-icons/io";
+import { IoMdSwitch, IoIosCheckmarkCircleOutline, IoMdLink } from "react-icons/io";
+import { FaRegCheckCircle, FaUserCog } from "react-icons/fa";
 import { CiCalendar } from "react-icons/ci";
 
 
@@ -32,14 +33,14 @@ export function TripDatailsPage() {
                     <div className="flex justify-between">
                         <h2 className="text-3xl font-semibold" >Atividades</h2>
                         <button className="bg-lime-300 text-lime-950 rounded-lg py-2 px-5 font-medium flex items-center gap-2 hover:bg-lime-400">
-                            <FiPlus className="size-5"/>
+                            <FiPlus className="size-5" />
                             Continuar
                         </button>
                     </div>
 
                     <div className="space-y-8">
                         <div className="space-y-2.5" >
-                            <div className="flex gap-2 items-baseline"> 
+                            <div className="flex gap-2 items-baseline">
                                 <span className="text-xl text-zinc-300 font-semibold" >Dia 17</span>
                                 <span className="text-xs text-zinc-500">Sábado</span>
                             </div>
@@ -48,7 +49,7 @@ export function TripDatailsPage() {
                         </div>
 
                         <div className="space-y-2.5" >
-                            <div className="flex gap-2 items-baseline"> 
+                            <div className="flex gap-2 items-baseline">
                                 <span className="text-xl text-zinc-300 font-semibold" >Dia 18</span>
                                 <span className="text-xs text-zinc-500">Domingo</span>
                             </div>
@@ -56,7 +57,7 @@ export function TripDatailsPage() {
                             <div className="space-y-2.5">
                                 <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex justify-between">
                                     <div className="flex items-center gap-2">
-                                        <IoIosCheckmarkCircleOutline className="size-5 text-lime-300"/>
+                                        <IoIosCheckmarkCircleOutline className="size-5 text-lime-300" />
                                         <span className="text-zinc-100">Corrida de kart</span>
                                     </div>
                                     <span className="text-sm text-zinc-400" >18:00h</span>
@@ -69,9 +70,15 @@ export function TripDatailsPage() {
                 <div className="w-80 space-y-6">
                     <div className="space-y-6">
                         <h2 className="font-semibold text-xl" >Links importantes</h2>
-                        
+
                         <div className="space-y-5">
-                            
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block flex-1 font-medium text-zinc-100">Reserva do AirBnB</span>
+                                    <a href="#" className="block truncate text-xs text-zinc-400">https://www.airbnb.com.br/rooms/104700011</a>
+                                </div>
+                                <IoMdLink className="size-6 text-zinc-400" />
+                            </div>
                         </div>
 
                         <button className="w-full bg-zinc-800 text-zinc-200 rounded-lg py-2 px-5 font-medium flex items-center gap-2 justify-center hover:bg-zinc-700">
@@ -82,7 +89,24 @@ export function TripDatailsPage() {
 
                     <div className="w-full h-px bg-zinc-800"></div>
 
-                    <div></div>
+                    <div className="space-y-6">
+                        <h2 className="font-semibold text-xl" >Convidados</h2>
+
+                        <div className="space-y-5">
+                            <div className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5">
+                                    <span className="block flex-1 font-medium text-zinc-100">Jessica White</span>
+                                    <span className="block truncate text-sm text-zinc-400">jessica.white44@yahoo.com</span>
+                                </div>
+                                <FaRegCheckCircle className="size-5 text-zinc-400" />
+                            </div>
+                        </div>
+
+                        <button className="w-full bg-zinc-800 text-zinc-200 rounded-lg py-2 px-5 font-medium flex items-center gap-2 justify-center hover:bg-zinc-700">
+                            <FaUserCog className="size-5" />
+                            Gerenciar convidados
+                        </button>
+                    </div>
                 </div>
 
             </main>
