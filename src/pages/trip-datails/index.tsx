1c660e224079596a0086e2954aab208bcc5b4cdc
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-07-11 17:49:43 -0300

    criando o modal de cadastrar atividades

--- src/pages/trip-datails/index.tsx
+++ src/pages/trip-datails/index.tsx
@@ -1,10 +1,23 @@
-import { FiMapPin, FiPlus } from "react-icons/fi";
-import { IoMdSwitch, IoIosCheckmarkCircleOutline, IoMdLink } from "react-icons/io";
+import { useState } from "react";
+
+import { FiMapPin, FiPlus, FiTag } from "react-icons/fi";
+import { IoMdSwitch, IoIosCheckmarkCircleOutline, IoMdLink, IoMdClose } from "react-icons/io";
 import { FaRegCheckCircle, FaUserCog } from "react-icons/fa";
 import { CiCalendar } from "react-icons/ci";
 
 
+
 export function TripDatailsPage() {
+    const [isCreateActiveModalOpen, setIsCreateActiveModalOpen] = useState(false);
+
+    function openCreateActiveModal() {
+        setIsCreateActiveModalOpen(true);
+    }
+
+    function closeCreateActiveModal() {
+        setIsCreateActiveModalOpen(false);
+    }
+
     return (
         <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
             <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
@@ -32,7 +45,7 @@ export function TripDatailsPage() {
                 <div className="flex-1 space-y-6">
                     <div className="flex justify-between">
                         <h2 className="text-3xl font-semibold" >Atividades</h2>
-                        <button className="bg-lime-300 text-lime-950 rounded-lg py-2 px-5 font-medium flex items-center gap-2 hover:bg-lime-400">
+                        <button onClick={openCreateActiveModal} className="bg-lime-300 text-lime-950 rounded-lg py-2 px-5 font-medium flex items-center gap-2 hover:bg-lime-400">
                             <FiPlus className="size-5" />
                             Continuar
                         </button>
@@ -82,7 +95,7 @@ export function TripDatailsPage() {
                         </div>
 
                         <button className="w-full bg-zinc-800 text-zinc-200 rounded-lg py-2 px-5 font-medium flex items-center gap-2 justify-center hover:bg-zinc-700">
-                            <FiPlus className="size-5" />
+                            <CiCalendar className="size-5" />
                             Alterar local/data
                         </button>
                     </div>
@@ -110,6 +123,40 @@ export function TripDatailsPage() {
                 </div>
 
             </main>
+
+            {isCreateActiveModalOpen && (
+                <div className="fixed inset-0 bg-black/60 flex items-center justify-center" >
+                    <div className="bg-zinc-900 w-[640px] rounded-xl py-5 px-6 shadow-shape space-y-5">
+
+                        <div className="space-y-2">
+                            <div className="flex justify-between items-center">
+                                <p className="text-lg font-semibold" >Cadastrar atividade</p>
+                                <button onClick={closeCreateActiveModal} ><IoMdClose className="size-6" /></button>
+                            </div>
+                            <p className="text-sm text-zinc-400" >Todos convidados podem visualizar as atividades.</p>
+                        </div>
+
+                        <form className="space-y-2">
+
+                            <div className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                                <FiTag className="size-6 text-zinc-400" />
+                                <input type="text" name='title' placeholder="Qual a atividade?" className="w-full bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+                            </div>
+
+                            <div className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                                <CiCalendar className="size-6 text-zinc-400" />
+                                <input type="datetime-local" name='time' placeholder="Qual a atividade?" className="w-full bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+                            </div>
+
+
+                            <button className="w-full bg-lime-300 text-lime-950 rounded-lg py-1.5 px-2 flex items-center justify-center hover:bg-lime-400">
+                                <span>Salvar atividade</span>
+                            </button>
+
+                        </form>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
