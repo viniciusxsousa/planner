032a513d5ba0862c6b58ed1080f88225edc5eaeb
Vinicius Sousa <viniciusxsousa@outlook.com>
2024-07-14 18:22:53 -0300

    exibindo as atividades da viagem

--- src/pages/trip-datails/actives.tsx
+++ src/pages/trip-datails/actives.tsx
@@ -2,6 +2,8 @@ import { api } from "../../api/api";
 
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale"
 
 import { IoIosCheckmarkCircleOutline } from "react-icons/io";
 
@@ -16,9 +18,9 @@ interface Actives {
 
 
 export function Actives() {
-    const [ actives, setActives ] = useState< Actives[] | undefined >();
+    const [actives, setActives] = useState<Actives[] | undefined>();
 
-    const {idTrip} = useParams();
+    const { idTrip } = useParams();
 
     async function getActives() {
         const response = await api.get(`/trips/${idTrip}/activities`);
@@ -32,16 +34,34 @@ export function Actives() {
 
     return (
         <div className="space-y-8">
-            <div className="space-y-2.5" >
-                <div className="flex gap-2 items-baseline">
-                    <span className="text-xl text-zinc-300 font-semibold" >Dia 17</span>
-                    <span className="text-xs text-zinc-500">Sábado</span>
-                </div>
+            {
+                actives?.map(category => (
+                    <div key={category.date} className="space-y-2.5" >
+                        <div className="flex gap-2 items-baseline">
+                            <span className="text-xl text-zinc-300 font-semibold" >Dia {format(category.date, 'd')}</span>
+                            <span className="text-xs text-zinc-500">{format(category.date, 'EEEE', { locale: ptBR })}</span>
+                        </div>
 
-                <p className="text-zinc-500 text-sm" >Nenhuma atividade cadastrada nessa data.</p>
-            </div>
+                        {
+                            category.activities.length > 0 ? (
+                                category.activities.map(active => (
+                                    <div key={active.id} className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex justify-between">
+                                        <div className="flex items-center gap-2">
+                                            <IoIosCheckmarkCircleOutline className="size-5 text-lime-300" />
+                                            <span className="text-zinc-100">{active.title}</span>
+                                        </div>
+                                        <span className="text-sm text-zinc-400" >{format(active.occurs_at, 'HH:mm')}</span>
+                                    </div>
+                                ))
+                            ) : (
+                                <p className="text-zinc-500 text-sm" >Nenhuma atividade cadastrada nessa data.</p>
+                            )
+                        }
+                    </div>
+                ))
+            }
 
-            <div className="space-y-2.5" >
+            {/*  <div className="space-y-2.5" >
                 <div className="flex gap-2 items-baseline">
                     <span className="text-xl text-zinc-300 font-semibold" >Dia 18</span>
                     <span className="text-xs text-zinc-500">Domingo</span>
@@ -56,7 +76,7 @@ export function Actives() {
                         <span className="text-sm text-zinc-400" >18:00h</span>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
